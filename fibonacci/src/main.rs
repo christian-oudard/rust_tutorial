6d794a1fab23c55a13a187cd4b3deaf10b70a8ea
Christian Oudard <christian.oudard@gmail.com>
2021-05-26 14:40:23 -0700

    Minor style fixes.

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -7,15 +7,22 @@ fn main() {
 
 #[cached]
 fn fib(n: u32) -> u32 {
-    if n == 1 { return 1; }
-    if n == 2 { return 1; }
-    return fib(n-1) + fib(n-2);
+    if n == 1 {
+        return 1;
+    }
+    if n == 2 {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
 }
 
-
 fn fib_iter(n: u32) -> u32 {
-    if n == 0 { return 0; }
-    if n == 1 { return 1; }
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
     let (mut a, mut b) = (0, 1);
     let mut c: u32;
     for _ in 2..=n {
@@ -25,4 +32,3 @@ fn fib_iter(n: u32) -> u32 {
     }
     return b;
 }
-
