6ed823fc26dda28cccf45ffc9ce2ae5247c7784e
Christian Oudard <christian.oudard@gmail.com>
2021-07-06 10:48:16 -0700

    Remove usage of extern crate.

--- logging/src/main.rs
+++ logging/src/main.rs
@@ -1,7 +1,12 @@
-#[macro_use]
-extern crate log;
+// RUST_LOG=trace cargo run
+
+use log::{trace, debug, info, warn, error};
 
 fn main() {
     env_logger::init();
+    trace!("logger initialized");
+    debug!("about to send info message");
     info!("Hello, logger.");
+    warn!("Maybe gonna send an error message");
+    error!("Problem!");
 }
