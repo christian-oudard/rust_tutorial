234a5669e4988d50129a4bf56ef9cb872603aecc
Christian Oudard <christian.oudard@gmail.com>
2021-09-16 14:21:28 -0700

    Implement a display of Pascal's Triangle.

--- scratch/src/bin/uninit.rs
+++ scratch/src/bin/uninit.rs
@@ -1,11 +1,13 @@
+
 use std::mem::MaybeUninit;
 
+#[allow(unreachable_code)]
 fn main() {
     let x = MaybeUninit::<u16>::uninit();
     let x = unsafe { x.assume_init() };
     dbg!(x);
     g();
-    f();
+    f(); 
 }
 
 fn f() -> ! {
