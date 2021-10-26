7a44c1494a20a68ad25f92cc5777a9c6420fa501
Christian Oudard <christian.oudard@gmail.com>
2021-10-26 16:19:57 -0700

    Testing Result::ok() and Result::err().

--- basic/src/bin/option.rs
+++ basic/src/bin/option.rs
@@ -1,7 +1,22 @@
+
+type Result = std::result::Result<String, ()>;
+
 fn main() {
     println!("{:?}", maybe_parse_int(None));
     println!("{:?}", maybe_parse_int(Some("123".into())));
     println!("{:?}", maybe_parse_int(Some("invalid".into())));
+
+    println!();
+    let res_ok: Result = Ok("stuff".into());
+    let res_err: Result = Err(());
+    println!("{:?}", res_ok.ok());
+    println!("{:?}", res_err.ok());
+
+    println!();
+    let res_ok: Result = Ok("stuff".into());
+    let res_err: Result = Err(());
+    println!("{:?}", res_ok.err());
+    println!("{:?}", res_err.err());
 }
 
 fn maybe_parse_int(maybe_s: Option<String>) -> Option<i64> {
