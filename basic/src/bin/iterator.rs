2572a96195e82bcf2cdfc2f764a97cc40d120774
Christian Oudard <christian.oudard@gmail.com>
2022-01-26 12:13:44 -0800

    Example of filter_map.

---
+++ basic/src/bin/iterator.rs
@@ -0,0 +1,8 @@
+fn main() {
+    dbg!((0..5).into_iter().map(|x| x * 2).collect::<Vec<_>>());
+    dbg!((0..10).into_iter().filter(|x| x % 2 == 1).collect::<Vec<_>>());
+
+    let v = vec![None, Some(1), Some(2), None, Some(4)];
+    dbg!(v.iter().filter(|x| x.is_some()).collect::<Vec<_>>());
+    dbg!(v.iter().filter_map(|x| *x).collect::<Vec<_>>());
+}
