e133ee4b2e9e986e9064a915cdcb0c98da165de8
Christian Oudard <christian.oudard@gmail.com>
2022-03-08 15:30:05 -0800

    Experiment with integer type conversions, and twos complement representation.

--- scratch/src/bin/overflow.rs
+++ scratch/src/bin/overflow.rs
@@ -1,17 +1,20 @@
 fn main() {
-    dbg!(10u8 - 2);
-    dbg!(10u8.checked_sub(2));
-    dbg!(10u8.saturating_sub(2));
-    dbg!(10u8.overflowing_sub(2));
-    dbg!(10u8.wrapping_sub(2));
+    // println!("{:?}", 10u8 - 12);
+    println!("{:?}", 10u8.checked_sub(12));
+    println!("{:?}", 10u8.saturating_sub(12));
+    println!("{:?}", 10u8.overflowing_sub(12));
+    println!("{:?}", 10u8.wrapping_sub(12));
 
-    dbg!(10u8.checked_sub(12));
-    dbg!(10u8.saturating_sub(12));
-    dbg!(10u8.overflowing_sub(12));
-    dbg!(10u8.wrapping_sub(12));
+    println!("");
 
-    #[allow(arithmetic_overflow)]
-    {
-        dbg!(10u8 - 12);
-    }
+    println!("{:?}", 10u8 - 2);
+    println!("{:?}", 10u8.checked_sub(2));
+    println!("{:?}", 10u8.saturating_sub(2));
+    println!("{:?}", 10u8.overflowing_sub(2));
+    println!("{:?}", 10u8.wrapping_sub(2));
+
+    println!("");
+
+    println!("{:?}", -10i8 as u8);
+    println!("{:?}", 246u8 as i8);
 }
