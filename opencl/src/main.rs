1186ece1275128f26867dd80a8381736c50f298c
Christian Oudard <ChristianOudard@pm.me>
2022-10-05 13:20:43 -0600

    Trying out OpenCL

---
+++ opencl/src/main.rs
@@ -0,0 +1,33 @@
+use ocl::ProQue;
+
+fn main() {
+    trivial().unwrap();
+}
+
+fn trivial() -> ocl::Result<()> {
+    let src = r#"
+        __kernel void add(__global float* buffer, float scalar) {
+            buffer[get_global_id(0)] += scalar;
+        }
+    "#;
+
+    let pro_que = ProQue::builder()
+        .src(src)
+        .dims(1 << 20)
+        .build()?;
+
+    let buffer = pro_que.create_buffer::<f32>()?;
+
+    let kernel = pro_que.kernel_builder("add")
+        .arg(&buffer)
+        .arg(10.0f32)
+        .build()?;
+
+    unsafe { kernel.enq()?; }
+
+    let mut vec = vec![0.0f32; buffer.len()];
+    buffer.read(&mut vec).enq()?;
+
+    println!("The value at index [{}] is now '{}'!", 123456, vec[123456]);
+    Ok(())
+}
