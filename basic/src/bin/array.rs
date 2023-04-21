858eda91b4bc480676ade15724641c43945b86af
Christian Oudard <ChristianOudard@pm.me>
2023-04-21 11:40:31 -0600

    Working with arrays and iterators.

---
+++ basic/src/bin/array.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // Create a simple array.
+    let a = [0u8; 16];
+    dbg!(a);
+}
--- basic/src/bin/iterator.rs
+++ basic/src/bin/iterator.rs
@@ -1,4 +1,8 @@
 fn main() {
+    dbg!((0..5).into_iter().collect::<Vec<_>>());
+    dbg!((5..0).into_iter().collect::<Vec<_>>());
+    dbg!((0..5).rev().into_iter().collect::<Vec<_>>());
+
     dbg!((0..5).into_iter().map(|x| x * 2).collect::<Vec<_>>());
     dbg!((0..10).into_iter().filter(|x| x % 2 == 1).collect::<Vec<_>>());
 
@@ -7,4 +11,7 @@ fn main() {
     dbg!(v.iter().filter_map(|x| *x).collect::<Vec<_>>());
 
     dbg!(vec!["a", "b", "c"].into_iter().collect::<String>());
+
+    dbg!(std::iter::zip(0..5, (0..5).rev()).collect::<Vec<(_,_)>>());
+    dbg!(std::iter::zip(0..5, (0..5).rev()).map(|(a, b)| (a, b, a+b)).collect::<Vec<(_,_,_)>>());
 }
