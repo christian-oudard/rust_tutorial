3d13364ae2379019940d3604be3f6dd555243593
Christian Oudard <ChristianOudard@pm.me>
2024-10-07 14:34:03 -0600

    Examining passing arrays and vecs of strings.

--- scratch/src/bin/random.rs
+++ scratch/src/bin/random.rs
@@ -2,10 +2,21 @@ use rand::prelude::*;
 
 fn main() {
     let mut rng = thread_rng();
+
     let x: f64 = rng.gen();
     dbg!(x);
 
     let mut nums: Vec<i32> = (0..10).collect();
     nums.shuffle(&mut rng);
     dbg!(nums);
+
+    dbg!(roll_d6(&mut rng));
+}
+
+//fn roll_d6<R: Rng>(rng: &mut R) -> u8 {
+//    (1..=6).choose(rng).unwrap()
+//}
+
+fn roll_d6<R: Rng>(rng: &mut R) -> u8 {
+    (1..=6).choose(rng).unwrap()
 }
